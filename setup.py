ea17674e9871981a26b086d43ba7d132b7c3de42
Dan <dstein64@users.noreply.github.com>
2020-07-24 19:06:49 -0400

    Use custom build extension.
    To append clang-specific compiler flags.

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 import os
 from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 
-# '-std=c++11' was added to `extra_compile_args` so the code can compile with clang++.
-# '-stdlib=libc++' was added to `extra_compile_args` and `extra_link_args` so the code
-# can compile on macOS with Anaconda.
-extension = Extension(
-    'kmeans1d._core',
-    ['kmeans1d/_core.cpp'],
-    extra_compile_args=['-std=c++11', '-stdlib=libc++'],
-    extra_link_args=['-stdlib=libc++']
-)
+
+class BuildExt(build_ext):
+    """A custom build extension for adding -std and -stdlib arguments for clang++."""
+    def is_using_clang(self):
+        cxx = self.compiler.compiler_cxx[0]
+        if 'clang' in cxx:
+            return True
+        # TODO: check if 'cxx' points to clang++.
+        return False
+
+    def build_extensions(self):
+        if self.is_using_clang():
+            for extension in self.extensions:
+                # '-std=c++11' is added to `extra_compile_args` so the code can compile
+                # with clang++. '-stdlib=libc++' is added to `extra_compile_args` and
+                # `extra_link_args` so the code can compile on macOS with Anaconda.
+                extension.extra_compile_args.extend(('-std=c++11', '-stdlib=libc++'))
+                extension.extra_link_args.append('-stdlib=libc++')
+        build_ext.build_extensions(self)
+
+
+extension = Extension('kmeans1d._core', ['kmeans1d/_core.cpp'])
 
 version_txt = os.path.join(os.path.dirname(__file__), 'kmeans1d', 'version.txt')
 with open(version_txt, 'r') as f:
@@ -19,19 +33,20 @@ setup(
     author='Daniel Steinberg',
     author_email='ds@dannyadam.com',
     classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'Topic :: Scientific/Engineering',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence',
-          'Topic :: Scientific/Engineering :: Information Analysis',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: Unix',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: MacOS',
-          'Operating System :: Microsoft :: Windows',
-          'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
     ],
+    cmdclass={'build_ext': BuildExt},
     description='A Python package for optimal 1D k-means clustering',
     ext_modules=[extension],
     keywords=['k-means', 'machine learning', 'optimization'],
