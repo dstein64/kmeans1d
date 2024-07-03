b508f44c8ca36d098995ecfdf8a503f424bc0c4f
Dan <dstein64@users.noreply.github.com>
2024-07-03 13:14:45 -0400

    Use the Limited API
    This way, a single wheel can be used across Python versions.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import os
+
 import setuptools
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
+
+PY_LIMITED_API_MAJOR = 3
+PY_LIMITED_API_MINOR = 2
 
 
 class BuildExt(build_ext):
@@ -25,7 +29,14 @@ class BuildExt(build_ext):
             build_ext.build_extensions(self)
 
 
-extension = Extension('kmeans1d._core', ['kmeans1d/_core.cpp'])
+extension = Extension(
+    'kmeans1d._core',
+    ['kmeans1d/_core.cpp'],
+    define_macros=[
+        ('Py_LIMITED_API', f'0x{PY_LIMITED_API_MAJOR:02x}{PY_LIMITED_API_MINOR:02x}0000')
+    ],
+    py_limited_api=True,
+)
 
 version_txt = os.path.join(os.path.dirname(__file__), 'kmeans1d', 'version.txt')
 with open(version_txt, 'r') as f:
@@ -59,6 +70,9 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     name='kmeans1d',
+    options={'bdist_wheel':
+        {'py_limited_api': f'cp{PY_LIMITED_API_MAJOR}{PY_LIMITED_API_MINOR}'}
+    },
     package_data={'kmeans1d': ['version.txt']},
     packages=['kmeans1d'],
     python_requires='>=3.6',
